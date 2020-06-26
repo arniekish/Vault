677498dbe010df65829cfff0b0212a933b553323
arniekish <64420865+arniekish@users.noreply.github.com>
2020-06-26 11:28:16 -0500

    Mini

---
+++ 10eefd0ae26f971293745d0e6ab1114dc46283c9/slide/SL_B6E872E8B4D8619C/slide.js
@@ -0,0 +1,28 @@
+﻿{ controls: {"CTL28A260882C6899B1":{"id": "CTL28A260882C6899B1"}
+,"CTL28E70F68FB121E9F":{"h": 62,
+ "hidden": 1,
+ "id": "CTL28E70F68FB121E9F",
+ "image": { "id": "group!GR_FD0322FEAA739682:3369994198B62DA4.png", "w": 66, "h": 62 },
+ "w": 66,
+ "y": 418}
+,"CTL2DDBC85204DC69B7":{"h": 121,
+ "id": "CTL2DDBC85204DC69B7",
+ "text": "<p style=\"text-align:left; \"><span class=\"book_page_title_1\" style=\"font-family:'BentonSans Light', Arial; font-size:20px; color:#5a5a5a; \">Learn&nbsp;how to:<\/span><\/p>\n<ul style=\"text-align:left; \">\n<li><span class=\"book_page_title_1\" style=\"font-family:'BentonSans Light', Arial; font-size:20px; color:#5a5a5a; \">Create a purchase requisition<\/span><\/li>\n<\/ul>\n<p>&nbsp;<\/p>\n<p style=\"text-align:left; \"><span class=\"book_page_title_1\" style=\"font-family:'BentonSans Light', Arial; font-size:20px; color:#5a5a5a; \">Duration: 2:00 min<\/span><\/p>\n",
+ "y": 624}
+,"CTL2E7E3EDCB7BFE086":{"id": "CTL2E7E3EDCB7BFE086"}
+,"CTL4769CEC295F8F5A3":{"id": "CTL4769CEC295F8F5A3",
+ "text": "<p><span class=\"book_page_title\" style=\"font-family:'BentonSans Light', Arial; font-size:64px; color:#5a5a5a; \">Creating Purchase Requisitions<\/span><\/p>\n"}
+,"CTL549A8E61219B2FAA":{"h": 46,
+ "href": "project!PR_E8D2A5158D5D9EBA:hands_on.pdf",
+ "id": "CTL549A8E61219B2FAA",
+ "y": 758}
+,"CTL5E7A8FD06B801390":{"id": "CTL5E7A8FD06B801390"}
+,"CTL7529CF301DC58582":{"id": "CTL7529CF301DC58582"}
+,"CTL76A4C8C4436663AC":{"id": "CTL76A4C8C4436663AC"}
+,"CTL86E55596EF14AE9D":{"id": "CTL86E55596EF14AE9D"}
+,"CTL9A2D8F5DA98B5AAD":{"id": "CTL9A2D8F5DA98B5AAD"}
+,"CTLB86EF7FDEF824499":{"id": "CTLB86EF7FDEF824499"}
+,"CTLBC0D32647D1B34AC":{"id": "CTLBC0D32647D1B34AC"}
+,"CTLC4F2936ACB3756A7":{"id": "CTLC4F2936ACB3756A7"}
+,"page":{"id": "page",
+ "template": "slide!SL_958210E16BB46E82"}}}
---
+++ 10eefd0ae26f971293745d0e6ab1114dc46283c9/slide/SL_C342666F25D73690/slide.js
@@ -0,0 +1,29 @@
+﻿{ controls: {"CTL139C32474E73FAB4":{"h": 53,
+ "hidden": 0,
+ "href": "project!PR_E8D2A5158D5D9EBA:hands_on.pdf",
+ "id": "CTL139C32474E73FAB4",
+ "y": 750}
+,"CTL28A260882C6899B1":{"id": "CTL28A260882C6899B1"}
+,"CTL2E7E3EDCB7BFE086":{"id": "CTL2E7E3EDCB7BFE086"}
+,"CTL4769CEC295F8F5A3":{"id": "CTL4769CEC295F8F5A3",
+ "text": "<p><span class=\"book_page_title\" style=\"font-family:'BentonSans Light', Arial; font-size:64px; color:#5a5a5a; \">Creating Purchase Requisitions<\/span><\/p>\n",
+ "y": 336}
+,"CTL5E7A8FD06B801390":{"id": "CTL5E7A8FD06B801390"}
+,"CTL63C4EF80ACE63CBB":{"id": "CTL63C4EF80ACE63CBB"}
+,"CTL6C219C8E2B204087":{"hidden": 0,
+ "id": "CTL6C219C8E2B204087"}
+,"CTL76A4C8C4436663AC":{"id": "CTL76A4C8C4436663AC"}
+,"CTL86E55596EF14AE9D":{"id": "CTL86E55596EF14AE9D"}
+,"CTL8FC34F217F68E083":{"id": "CTL8FC34F217F68E083"}
+,"CTL9753C660C054C4AE":{"id": "CTL9753C660C054C4AE"}
+,"CTL9A2D8F5DA98B5AAD":{"id": "CTL9A2D8F5DA98B5AAD"}
+,"CTLB86EF7FDEF824499":{"id": "CTLB86EF7FDEF824499"}
+,"CTLBF1B668FE8DF04B0":{"id": "CTLBF1B668FE8DF04B0"}
+,"CTLCA9A8D47B310AFA9":{"hidden": 0,
+ "id": "CTLCA9A8D47B310AFA9"}
+,"CTLFFE7072667AD749D":{"h": 62,
+ "id": "CTLFFE7072667AD749D",
+ "text": "<p style=\"text-align:left; \"><span class=\"book_page_title_1\">You have seen,&nbsp;how&nbsp;to: <\/span><\/p>\n<ul>\n<li><span class=\"book_page_title_1\" style=\"font-family:'BentonSans Light', Arial; \">Create a purchase requisition<\/span><\/li>\n<\/ul>\n",
+ "y": 687}
+,"page":{"id": "page",
+ "template": "slide!SL_6F96F652C744BB97"}}}
